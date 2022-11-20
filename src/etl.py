421f859ad09a2106abc2dd4640a0bee25b067f5e
Kenneth (Kenny) Nguyen <62914814+ProfessorMomo@users.noreply.github.com>
2022-11-20 14:21:04 -0800

    reading test and train data here

--- src/etl.py
+++ src/etl.py
@@ -1,1 +1,31 @@
+import os
+import pandas as pd
+from sklearn.impute import SimpleImputer
 
+def get_data(datadir):
+    '''
+    load and clean the training data
+    '''
+    file_path = os.path.join(datadir, 'patient_3_sleep.csv')
+    
+    data = pd.read_csv(file_path) #check to make sure it ends in .csv
+    labels = data['summary_date'].apply(pd.to_datetime).dt.day_name()
+    data = data.drop(columns = ['timestamp', 'hypnogram_5min', 'summary_date', 'rmssd', 'type'])
+    data_cols = data.columns
+    imp_mean = SimpleImputer(missing_values=np.nan, strategy='mean')
+    imp_mean.fit(data)
+    data = imp_mean.transform(data)
+    data = pd.DataFrame(data, columns = data_cols)
+    
+    return pd.concat([data, labels], axis = 1)
+  
+  
+def read_test(datadir):
+    '''
+    Reads raw test data from disk.
+    (Would normally be more complicated!)
+    '''
+
+    fp = os.path.join(datadir, 'test.csv')
+    return pd.read_csv(fp)
+    
